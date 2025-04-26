93f7b9cb724e99e2d3f45d8e22a3602529eed447
Ahmed-eldaker <ahmeddaker01@gmail.com>
2025-04-26 18:22:38 +0300

    update

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
+// const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const Port = process.env.Port || 3000;
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.use("/auth", require("./routes/auth"));
